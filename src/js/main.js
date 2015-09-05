953c2d3bc7da8fdc49692096cd11e8dd17f9fe33
Peter West <peter.g.west@gmail.com>
2015-09-05 18:01:13 +0100

    basic tcx import

--- src/js/main.js
+++ src/js/main.js
@@ -6,7 +6,9 @@ var _ = require('lodash'),
 	uiRouter = require('angular-ui-router'),
 	PouchDB = require('pouchdb'),
 	d3 = require('d3'),
-	uuid = require('node-uuid');
+	uuid = require('node-uuid'),
+	X2JS = require('xml-json-parser'),
+	jsonPath = require('JSONPath');
 
 PouchDB.plugin(require('transform-pouch'));
 PouchDB.plugin({
@@ -388,20 +390,78 @@ angular.module('traq', [ngMaterial, uiRouter])
 				url: '/import',
 				templateUrl: 'import.html',
 				controller: function ($scope, $state) {
-					var dateColumnNames = ['date'];
+					var dateColumnNames = ['date', 'time'];
+
+					// { name: { first: 'John', 'last': 'Smith' } }
+					// --> { namefirst: 'John', namelast: 'Smith' }
+					var flattenObj = function (obj) {
+						var newObj = {};
+						_.each(obj, function (val, key) {
+							if (_.isObject(val)) {
+								_.each(flattenObj(val), function (val, key2) {
+									if (_.isObject(val)) {
+										newObj[key + key2] = val;
+									} else {
+										newObj[key + key2] = val;
+									}
+								});
+							} else {
+								newObj[key] = obj[key];
+							}
+						});
+						return newObj;
+					};
+
+					var parseNameUnit = function (name) {
+						var searchUnits = ['meters', 'metres', 'bpm', 'km'],
+							unit = _.find(searchUnits, function (unit) {
+								return name.toLocaleLowerCase().indexOf(unit) > -1;
+							});
+						if (unit) {
+							var i = name.toLocaleLowerCase().indexOf(unit);
+							name = name.slice(0, i) + name.slice(i + unit.length);
+						} else {
+							// brackets
+							var parts = name.match(/^(.*?)\s*(?:\(([^()]*)\))?\s*$/);
+							console.log(parts)
+							name = parts[1];
+							unit = parts[2];
+						}
+						console.log(name, unit);
+						return { name: name, unit: unit };
+					};
+
+					var importers = {
+						sv: function (contents, type) {
+							// fix: remove fitbit header
+							var lines = contents.split('\n');
+							if (lines[0].split(',').length === 1 && lines[1].split(',').length > 1) {
+								contents = lines.slice(1).join('\n');
+							}
+							// end fix
+							// fix: remove blank lines at end
+							contents = contents.trim();
+							// end fix
+							return d3[type].parse(contents);
+						},
+						csv: function (contents) { return importers.sv(contents, 'csv'); },
+						tsv: function (contents) { return importers.sv(contents, 'tsv'); },
+						tcx: function (contents) {
+							var json = (new X2JS()).xml_str2json(contents),
+								trackpoints = jsonPath(null, json, '$..Trackpoint')[0];
+							var data = _.map(trackpoints, function (trackpoint) {
+								return flattenObj(trackpoint);
+							});
+							console.log(data);
+							return data;
+						}
+					};
+
 					$scope.import = {};
 					$scope.$watch('import.contents', function (contents) {
 						if (!contents) { return; }
-						// fix: remove fitbit header
-						var lines = contents.split('\n');
-						if (lines[0].split(',').length === 1 && lines[1].split(',').length > 1) {
-							contents = lines.slice(1).join('\n');
-						}
-						// end fix
-						// fix: remove blank lines at end
-						contents = contents.trim();
-						// end fix
-						var data = d3.csv.parse(contents),
+
+						var data = importers[$scope.import.format](contents),
 							keys = _.keys(data[0]),
 							dateKey = _.find(keys, function (key) {
 								return dateColumnNames.indexOf(key.trim().toLocaleLowerCase()) > -1;
@@ -411,12 +471,12 @@ angular.module('traq', [ngMaterial, uiRouter])
 						}
 						// fix: try to normalise dates to MM-dd-YYYY :(
 						var isDDMMYYYY = _.reduce(data, function (memo, row) {
-							return memo || Number(row[dateKey].slice(0, 2)) > 12;
+							var parts = row[dateKey].split(/[-\\\/ ]/);
+							return parts[0] && (memo || Number(parts[0]) > 12 && Number(parts[0]) < 32); // isn't a month or year - must be a day
 						}, false);
 						if (isDDMMYYYY) {
 							_.each(data, function (row) {
 								row[dateKey] = row[dateKey].split(/[-\\\/ ]/).reverse().join('-');
-								console.log(row[dateKey])
 							});
 						}
 						// end fix
@@ -424,11 +484,11 @@ angular.module('traq', [ngMaterial, uiRouter])
 							_id: 'tbl[' + uuid.v4() + ']',
 							title: $scope.import.title,
 							columns: _.chain(keys).map(function (key) {
-								var parts = key.match(/^(.*?)\s*(?:\(([^()]*)\))?\s*$/);
+								var parts = parseNameUnit(key);
 								return {
 									id: 'col[' + uuid.v4() + ']',
-									name: parts[1],
-									unit: parts[2],
+									name: parts.name,
+									unit: parts.unit,
 									originalKey: key
 								};
 							}).reject(function (column) {
@@ -462,19 +522,34 @@ angular.module('traq', [ngMaterial, uiRouter])
 			template: '<input type="file">',
 			scope: {
 				contents: '=',
-				name: '='
+				name: '=',
+				format: '='
 			},
 			link: function (scope, element) {
+				var permittedFormats = {
+					csv: ['.csv', 'text/csv'],
+					tsv: ['.tsv'],
+					tcx: ['.tcx']
+				};
+
 				element.on('change', function () {
 					var reader = new FileReader(),
-						file = element[0].files[0];
+						file = element[0].files[0],
+						extension = file.name.indexOf('.') > -1 ? file.name.match(/\.[^.\/]+$/)[0] : '',
+						name = file.name.slice(0, -extension.length),
+						format = _.chain(permittedFormats).keys().find(function (format) {
+							return permittedFormats[format].indexOf(extension) > -1 ||
+								permittedFormats[format].indexOf(file.type) > -1;
+						});
 
-					if (file.type.indexOf('text') !== 0) {
-						console.error('File type not supported');
+					if (!format) {
+						console.error('File format not supported');
 						return;
 					}
+
 					reader.onload = function () {
-						scope.name = file.name.replace(/\.[^.\/]+$/, ''); // remove extension
+						scope.name = name;
+						scope.format = format;
 						scope.contents = reader.result;
 						scope.$apply();
 					};
