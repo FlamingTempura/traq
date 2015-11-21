9c9574d4f60336ba746187a000b73e6c04627f92
Peter West <peter.g.west@gmail.com>
2015-11-21 21:09:20 +0000

    fixed measurement delete undo

--- src/state/measurements.js
+++ src/state/measurements.js
@@ -1,9 +1,7 @@
 'use strict';
 
 var angular = require('angular'),
-	_ = require('lodash'),
-	moment = require('moment')/*,
-	emit = require('pouchdb').emit*/;
+	_ = require('lodash');
 
 angular.module('traq').config(function ($stateProvider) {
 	$stateProvider.state('measurements', {
@@ -30,17 +28,14 @@ angular.module('traq').config(function ($stateProvider) {
 							descending: true
 						});
 					})).then(function (columnResults) {
-						console.log('RS', columnResults)
 						var ids = _.chain(columnResults).pluck('rows').flatten().map(function (measurement) {
 							return {
 								id: measurement.id,
 								timestamp: Number(measurement.id.split(':')[1])
 							};
 						}).sortByOrder('timestamp', 'desc').slice(0, chunkSize).pluck('id').value();
-						console.log('gotIds');
 						return dbMeasurement.getAll({ keys: ids });
 					}).then(function (measurements) {
-						console.log('MEAS', measurements);
 						oldestTimestamp = _.last(measurements)._id.split(':')[1];
 						return _.map(measurements, function (measurement) {
 							return _.extend({}, measurement, {
@@ -48,7 +43,6 @@ angular.module('traq').config(function ($stateProvider) {
 							});
 						});
 					}).then(function (measurements) {
-						console.log('fetched chunk', chunkNumber);
 						chunks[chunkNumber] = measurements;
 					}).catch(function (err) {
 						console.error(err);
@@ -76,24 +70,23 @@ angular.module('traq').config(function ($stateProvider) {
 				$scope.data.selectMode = !!_.find(selected, function (selected) { return selected; });
 			}, true);
 			$scope.delete = function () {
-				$q.all(
-					_.chain($scope.data.selected).map(function (selected, key) {
-						var parts = key.split(':::');
-						return { id: parts[0], rev: parts[1], selected: selected };
-					}).select(function (o) {
-						return o.selected;
-					}).map(function (o) {
-						return dbMeasurement.remove(o.id, o.rev);
-					}).value()
-				).then(function () {
-					$state.go($state.current, {}, { reload: true });
+				var docs = _.chain($scope.data.selected).map(function (selected, key) {
+					var parts = key.split(':::');
+					return { id: parts[0], rev: parts[1], selected: selected };
+				}).select(function (o) {
+					return o.selected;
+				}).map(function (o) {
+					return { _id: o.id, _rev: o.rev, _deleted: true };
+				}).value();
+				dbMeasurement.bulkDocs(docs).then(function (result) {
+					if (result.length === 0) { return; }
+					var undoId = result[0].undoId;
 					snack('The measurement has been deleted', 'Undo', function () {
-						console.log('reviving', selected);
-						dbMeasurement.bulkDocs(_.map(selected, function (measurement) { // FIXME
-							return _.omit(measurement, '_rev');
-						}));
-						$state.go($state.current, {}, { reload: true });
+						dbMeasurement.undo(undoId).then(function () {
+							$state.go($state.current, {}, { reload: true });
+						});
 					});
+					$state.go($state.current, {}, { reload: true });
 				}).catch(function (err) {
 					// TODO
 					console.error(err);
